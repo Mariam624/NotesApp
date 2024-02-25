eed60c310bdb681d3ab8008c512c59ed3782763b
vahe.h.x@gmail.com <hyanmariam0@gmail.com>
2024-02-25 23:28:24 +0400

    Adding Room

--- app/src/main/java/com/example/notesapp/database/NoteDao.kt
+++ app/src/main/java/com/example/notesapp/database/NoteDao.kt
@@ -12,20 +12,19 @@ import com.example.notesapp.model.Note
 @Dao
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun  insertNote(note: Note)
+    suspend fun insertNote(note: Note)
 
     @Update
-    suspend fun  updateNote(note: Note)
+    suspend fun updateNote(note: Note)
 
     @Delete
-    suspend fun  deleteNote(note: Note)
+    suspend fun deleteNote(note: Note)
 
     @Query("SELECT * FROM NOTES ORDER BY id DESC")
     fun getAllNotes(): LiveData<List<Note>>
 
     @Query("SELECT * FROM NOTES WHERE noteTitle LIKE :query OR noteDesc LIKE :query")
     fun searchNote(query: String?): LiveData<List<Note>>
-
 
 
 }
